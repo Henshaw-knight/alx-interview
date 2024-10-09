341a42446b83fc86c6739a6d8ddbea33ab0d2789
Henshaw <josephhensh2000@gmail.com>
2024-10-09 11:09:53 -0700

    Implement solution to challenge: write method that determines if all the boxes can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,24 +6,14 @@ def canUnlockAll(boxes):
     """ Method that determines if all boxes can be opened or not
     Args:
       boxes: a list of lists
+    Returns:
+      True if all boxes can be opened, else False is returned.
     """
-    # List to hold the boolean result of every
-    # individual box after conditional check
-    # If the key of a box is found in another box, True otherwise
-    # False is returned
-    result = []
-    for i in range(0, len(boxes) - 1):
-        individual_box_result = []
-        for box in boxes:
-            # Remove the key that can unlock the box holding it
-            # to aid the checking
-            if i in boxes[i]:
-                boxes[i].remove(i)
-            if (i in box):
-                individual_box_result.append(True)
-            else:
-                individual_box_result.append(False)
-        individual_box_result = any(individual_box_result)
-        # for i == 0, True is appended because boxes[0] is always unlocked
-        result.append(individual_box_result) if i != 0 else result.append(True)
-    return all(result)
+    all_boxes = set()
+    unique_keys = {0}
+    for index, box in enumerate(boxes):
+        all_boxes.add(index)
+        for key in box:
+            if key != index:
+                unique_keys.add(key)
+    return True if unique_keys == all_boxes else False
