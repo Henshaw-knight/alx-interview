8027b6a05d0d8007204c1f1100521b38ed47ed15
Henshaw <josephhensh2000@gmail.com>
2024-10-17 16:29:40 +0000

    Update file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -20,7 +20,7 @@ def minOperations(n):
     basic_prime_nums = [2, 3, 5, 7, 11]
     result = []
     i = 0
-    while (n != 1 and n > 0 and type(n) == int):
+    while (n != 1 and n > 0 and (type(n) == int or type(n) == float)):
         divisor = basic_prime_nums[i]
 
         if (n % divisor == 0):
