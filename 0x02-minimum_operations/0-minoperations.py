c2a15d40202f25528919f66413ab2d24320148ff
Henshaw <josephhensh2000@gmail.com>
2024-10-17 16:12:39 +0000

    Devise solution that efficiently calculates the minimum number of operations to achieve a given number of characters using only 'Copy All' and 'Paste' operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""minOperations function module.
+
+Solved challenge using Prime Factorization
+"""
+
+
+def minOperations(n):
+    """
+      Calculates the minimum number of operations to achieve exactly n
+      H characters using only 'Copy All' and 'Paste' operations.
+
+      Args:
+        n (int): The target number of H characters.
+
+      Returns:
+        int: The fewest number of operations needed to reach n characters.
+             Returns 0 if n is impossible to achieve.
+    """
+    basic_prime_nums = [2, 3, 5, 7, 11]
+    result = []
+    i = 0
+    while (n != 1):
+        divisor = basic_prime_nums[i]
+
+        if (n % divisor == 0):
+            result.append(divisor)
+            n = n/divisor
+        else:
+            i += 1
+    return sum(result)
